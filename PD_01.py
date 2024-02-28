9356ca6c6aea51cbb0a28faed0e3906115eeb1d7
DawidSzoka1 <oficer130@gmail.com>
2024-02-28 16:06:20 +0100

    PD_01

---
+++ PD_01.py
@@ -0,0 +1,69 @@
+# # zad 1
+# list_names = ["dawid", "dawid", "dawid", "lukasz", "kuba", "seba", "edek", "ula", "franciszek", 'elzbieta']
+# name = input("Enter your name: ")
+#
+# indeks = list_names.index(name)
+# amount = list_names.count(name)
+# list_names.append("Mieszko")
+# list_names.insert(2, "Marek")
+# list_names.sort()
+# list_names.pop()
+# new_list = ["Mieszko", "Marek", "Pazdan"]
+# list_names = list_names + new_list
+#
+#
+# # sprawdzenie
+# print(0 > 1)
+# print(0 <= 1)
+# print(0 >= 1)
+# print(1 == 0)
+# print(1 == 1)
+# print(1 != 0)
+# print(1 != 1)
+
+# # zad 2
+# x = int(input("Enter a number x: "))
+# y = int(input("Enter a number y: "))
+# print(f"2x + 5y = {2*x + 5*y}")
+
+# # zad 3
+# a = input("Enter your name: ")
+# b = input("Enter your last name: ")
+# c = input("Enter your age: ")
+# d = input("Enter what you are studying: ")
+# print(f"Jestem {a} {b} mam {c} lat studiuję {d}")
+
+# # zad 4
+# print(
+#     f"Czy liczba 1+2+10+20000001+4+347586970885 =  321784560456434534646:"
+#     f" {1 + 2 + 10 + 20000001 + 4 + 347586970885 == 3217845604564346}"
+# )
+# # zad 5
+# num1 = int(input("Enter a number1: "))
+# num2 = int(input("Enter a number2: "))
+# print(f"Czy ich suma jest parzysta: {(num1 + num2 )% 2 == 0}")
+
+# # zad 6
+# x = int(input("Enter a number: "))
+# y = int(input("Enter second number: "))
+# print(f"x + y = {x + y}")
+# print(f"x - y = {x - y}")
+# print(f"x * y = {x * y}")
+# print(f"x / y = {x / y}")
+# print(f"x^y = {x ** y}")
+
+# # zad 7
+# x = int(input("Enter a number: "))
+# if 1 < x < 13:
+#     print("x miedzy 1 a 13")
+# elif x != 5 or x < 0:
+#     print("x != od 5 lub mniejszy od 0")
+
+
+
+
+
+
+
+
+
