831c95a8f2b34415dca00b9762ce88dc464c3205
DawidSzoka1 <oficer130@gmail.com>
2024-04-17 09:08:42 +0200

    added user input url and name of a product

--- projekt_dodtakowy/web_Screaping.py
+++ projekt_dodtakowy/web_Screaping.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-
 url = input("Podaj adres do aukcji na olx program znajdzie cene: ")
 page = requests.get(
     url,
@@ -9,13 +8,22 @@ page = requests.get(
 if page.status_code == 200:
     soup = bs(page.text, "html.parser")
     div_content_price = soup.find("div", {"data-testid": "ad-price-container"})
+    div_content_name = soup.find('div', {'data-cy': 'ad_title'})
     try:
-        print(list(div_content_price.children)[2].text)
+        price = list(div_content_price.children)[2].text
+        product_name = list(div_content_name.children)[-1].text
+        print(f"Produkt: {product_name} \nkosztuje: {price}")
     except AttributeError as e:
-        print(f"{e}")
+        raise AttributeError(e)
     except IndexError as e:
-        print(e)
+        raise IndexError(e)
     except TypeError as e:
-        print(e)
+        raise TypeError(e)
+
+    bugget = float(input("Podaj swoj budzet: "))
+    price = float(price.split('z')[0].replace(' ', ''))
+    if price > bugget:
+        pass
+
 else:
     print("Podano zly adres do aukcji na olx!")
