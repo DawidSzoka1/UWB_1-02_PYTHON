d414c3bcc37c24f9248f8e780977ff8c7c5bb633
DawidSzoka1 <oficer130@gmail.com>
2024-04-22 17:58:22 +0200

    added catching errors

--- lab_08_09/service_library/additionalfun.py
+++ lab_08_09/service_library/additionalfun.py
@@ -1,9 +1,27 @@
 import pandas as pd
 
 
-def read_csv(csv_file, *args):
-    return pd.read_csv(
-        f'Library/{csv_file}',
-        usecols=[*args],
-        index_col='ID'
-    )
+def read_csv(path_to_csv_file, *args):
+
+    if 'ID' not in args:
+
+        try:
+            return pd.read_csv(path_to_csv_file, usecols=[*args])
+        except FileNotFoundError as e:
+            print(f"File not found error occurred: {e}")
+            return None
+        except ValueError as e:
+            print(f"Value error occurred: {e}")
+            return None
+    try:
+        return pd.read_csv(
+            path_to_csv_file,
+            usecols=[*args],
+            index_col='ID'
+        )
+    except FileNotFoundError as e:
+        print(f"File not found error occurred: {e}")
+        return None
+    except ValueError as e:
+        print(f"Value error occurred: {e}")
+        return None
