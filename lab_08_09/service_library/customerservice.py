96b974da987bb22c46b46c3b539912e4c2cd5a8c
DawidSzoka1 <oficer130@gmail.com>
2024-04-22 18:05:21 +0200

    added doc string to add_customer

--- lab_08_09/service_library/customerservice.py
+++ lab_08_09/service_library/customerservice.py
@@ -3,6 +3,19 @@ from datetime import date
 
 
 def add_customer(name, email=None, phone_number=None):
+    """
+    Function to add a customer to an existing csv file (customer.csv) with
+    name and email and phone number and data of created and updated customer
+
+    Args:
+        name(str): The name of the customer:
+        email(str): The email of the customer:
+        phone_number(str): The phone number of the customer:
+
+    Returns:
+        0 if the customer phone_number is already taken or email is already taken
+        else 'Added Customer successfully'
+    """
     df = read_csv('Library/customer.csv',
                   'ID', 'NAME', 'E-MAIL', 'PHONE', 'CREATE', 'UPDATE')
     time = date.today()
@@ -12,11 +25,10 @@ def add_customer(name, email=None, phone_number=None):
     if email in df['E-MAIL']:
         print('E-mail is already taken')
         return 0
-    max_index = df.index[-1] + 1
+    max_index = int(df.index[-1]) + 1
     df.loc[max_index] = [name.title(), email, phone_number, time, time]
     df.to_csv('Library/customer.csv')
-    print('Added customer')
-
+    return 'Added Customer successfully'
 
 
 def update_customer(name, email, phone_number):
