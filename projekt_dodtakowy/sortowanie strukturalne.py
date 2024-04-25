350e5f6e21493e89467a2f86b8a11538aea4bea2
DawidSzoka1 <oficer130@gmail.com>
2024-04-25 14:42:06 +0200

    some structural programs not all working

--- projekt_dodtakowy/sortowanie strukturalne.py
+++ projekt_dodtakowy/sortowanie strukturalne.py
@@ -1,15 +1,38 @@
 def sort_list(array: list):
     sorted_list = False
-    right = len(array)-1
+    right = len(array) - 1
 
     while not sorted_list:
         sorted_list = True
         for i in range(0, right):
-            if array[i] > array[i+1]:
+            if array[i] > array[i + 1]:
                 sorted_list = False
-                array[i], array[i+1] = array[i+1], array[i]
+                array[i], array[i + 1] = array[i + 1], array[i]
 
 
 test = [2, 1, 3, 4, 0, 10, 9]
 sort_list(test)
 print(test)
+
+
+def find_in_sorted_list(array: list, find: int):
+    left = 0
+    right = len(array) - 1
+    finded = False
+    while not finded:
+        finded = True
+        mid = (left + right) // 2
+        if array[mid] == find:
+            return mid
+        elif array[mid] < find:
+            finded = False
+            left = mid
+            right += 1
+        elif array[mid] > find:
+            finded = False
+            right = mid
+            left -= 1
+    return "Brak"
+
+
+print(find_in_sorted_list(test, 0))
