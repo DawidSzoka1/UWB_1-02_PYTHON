b7bdcb66b147cd9fb64d37c355e801a2a12f25e4
DawidSzoka1 <oficer130@gmail.com>
2024-04-26 15:13:03 +0200

    if ID not in args error and some error handling in update_address and update_customer

--- lab_08_09/service_library/additionalfun.py
+++ lab_08_09/service_library/additionalfun.py
@@ -3,14 +3,7 @@ import pandas as pd
 
 def read_csv(path_to_csv_file, *args):
     if 'ID' not in args:
-        try:
-            return pd.read_csv(path_to_csv_file, usecols=[*args])
-        except FileNotFoundError as e:
-            print(f"File not found error occurred: {e}")
-            return e
-        except ValueError as e:
-            print(f"Value error occurred: {e}")
-            return e
+        return 0
     try:
         return pd.read_csv(
             path_to_csv_file,
@@ -19,14 +12,17 @@ def read_csv(path_to_csv_file, *args):
         )
     except FileNotFoundError as e:
         print(f"File not found error occurred: {e}")
-        return e
+        return 0
     except ValueError as e:
         print(f"Value error occurred: {e}")
-        return e
+        return 0
 
 
 def update_address(customer_id, df_address, street='', city='', country=''):
-    address = df_address[df_address['ID'] == customer_id]
+    if customer_id not in df_address.index:
+        print('Customer ID not found')
+        return 0
+    address = df_address.loc[customer_id]
     street = street.title() if street else address['STREET']
     city = city.title() if city else address['CITY']
     country = country if country else address['COUNTRY']
@@ -36,7 +32,10 @@ def update_address(customer_id, df_address, street='', city='', country=''):
 
 
 def update_customer(customer_id, df, name='', email='', phone_number=0):
-    user = df[df['ID'] == customer_id]
+    if customer_id not in df.index:
+        print('Customer ID not found')
+        return 0
+    user = df.loc[customer_id]
     name = name.title() if name else user['NAME']
     email = email if email else user['E-MAIL']
     phone_number = phone_number if phone_number else user['PHONE']
