2323e6a5d02451c496c361e14e75dd6d1b905743
DawidSzoka1 <oficer130@gmail.com>
2024-04-26 16:17:38 +0200

    try except while update customer and address

--- lab_08_09/service_library/additionalfun.py
+++ lab_08_09/service_library/additionalfun.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from datetime import date
 
+from pandas.errors import SettingWithCopyWarning, IndexingError
+
 
 def read_csv(path_to_csv_file, *args):
     if 'ID' not in args:
@@ -20,24 +22,46 @@ def read_csv(path_to_csv_file, *args):
 
 
 def update_address(customer_id, df_address, street='', city='', country=''):
-    if customer_id not in df_address.index:
-        return 0
     address = df_address.loc[customer_id]
     street = street.title() if street else address['STREET']
     city = city.title() if city else address['CITY']
     country = country if country else address['COUNTRY']
-    df_address.loc[customer_id] = [street.title(), city.title(), country]
-    df_address.to_csv('Library/address.csv')
-    return 1
+    try:
+        df_address.loc[customer_id] = [street.title(), city.title(), country]
+        df_address.to_csv('Library/address.csv')
+        return 1
+    except ValueError as e:
+        print("Value error occurred: ", e)
+        return 0
+    except TypeError as e:
+        print("Type error occurred: ", e)
+        return 0
+    except SettingWithCopyWarning as e:
+        print("SettingWithCopyWarning error occurred: ", e)
+        return 0
+    except IndexingError as e:
+        print("IndexingErrors error occurred: ", e)
+        return 0
 
 
 def update_customer(customer_id, df, name='', email='', phone_number=0):
-    if customer_id not in df.index:
-        return 0
     user = df.loc[customer_id]
     name = name.title() if name else user['NAME']
     email = email if email else user['E-MAIL']
     phone_number = phone_number if phone_number else user['PHONE']
-    df.loc[customer_id] = [name.title(), email, phone_number, df.loc[customer_id]['CREATE'], date.today()]
-    df.to_csv('Library/customer.csv')
-    return 1
+    try:
+        df.loc[customer_id] = [name.title(), email, phone_number, df.loc[customer_id]['CREATE'], date.today()]
+        df.to_csv('Library/customer.csv')
+        return 1
+    except ValueError as e:
+        print("Value error occurred: ", e)
+        return 0
+    except TypeError as e:
+        print("Type error occurred: ", e)
+        return 0
+    except SettingWithCopyWarning as e:
+        print("SettingWithCopyWarning error occurred: ", e)
+        return 0
+    except IndexingError as e:
+        print("IndexingErrors error occurred: ", e)
+        return 0
