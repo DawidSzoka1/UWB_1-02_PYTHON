7fd7373978c7958583119f4a5255e8680e6f2760
DawidSzoka1 <oficer130@gmail.com>
2024-04-28 15:54:33 +0200

    better validation in check_if_dataset and create_user_dataset

--- lab_08_09/service_library/additionaluserfun.py
+++ lab_08_09/service_library/additionaluserfun.py
@@ -1,9 +1,17 @@
 from datetime import date
 import os
+from additionalfun import *
+import pandas as pd
 from pandas.errors import SettingWithCopyWarning, IndexingError
 
 
 def update_address(customer_id, df_address, street='', city='', country=''):
+    if type(df_address) is not pd.DataFrame:
+        print('df_address is not a Pandas DataFrame')
+        return 0
+    if customer_id not in df_address.index:
+        print('customer_id is not in df_address')
+        return 0
     address = df_address.loc[customer_id]
     street = street.title() if street else address['STREET']
     city = city.title() if city else address['CITY']
@@ -50,27 +58,42 @@ def update_customer(customer_id, df, name='', email='', phone_number=0):
 
 
 def create_user_dataset(customer_id):
+    path = os.path.join(os.getcwd(), 'DATASET')
     if not os.path.exists('DATASET'):
         try:
-            os.mkdir(os.path.join(os.getcwd(), 'DATASET'))
+            os.mkdir(path)
         except PermissionError as e:
             print('Error ', e)
             return 0
         except OSError as e:
             print('Error ', e)
             return 0
-
-    os.chdir('DATASET')
-    with open(f'{customer_id}.txt', 'w') as f:
+    df = read_csv('Library/customer.csv',
+                  'ID', 'NAME', 'E-MAIL', 'PHONE', 'CREATE', 'UPDATE')
+    if not df:
+        return 0
+    elif customer_id not in df.index:
+        print('No such customer')
+        return 0
+    elif os.path.exists(os.path.join(path, f'{customer_id}.txt')):
+        return 1
+    with open(os.path.join(path, f'{customer_id}.txt'), 'w') as f:
         pass
+    return 1
 
 
 def check_if_dataset(customer_id):
     if not os.path.exists('DATASET'):
-        print('DATASET does not exist')
+        return create_user_dataset(customer_id)
+    df = read_csv('Library/customer.csv',
+                  'ID', 'NAME', 'E-MAIL', 'PHONE', 'CREATE', 'UPDATE')
+    path = os.path.join(os.getcwd(), 'DATASET')
+    if not df:
         return 0
-    os.chdir('DATABASE')
-    if not os.path.exists(f'{customer_id}.txt'):
-        print('There is no customer with that id')
+    elif customer_id not in df.index:
+        print('No such customer')
         return 0
+    elif not os.path.exists(os.path.join(path, f'{customer_id}.txt')):
+        with open(os.path.join(path, f'{customer_id}.txt'), 'w') as f:
+            pass
     return 1
