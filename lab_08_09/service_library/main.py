0169aceb324d9783290511b5cc68d88bf30dbc16
DawidSzoka1 <oficer130@gmail.com>
2024-04-30 15:27:47 +0200

    some label and entry to add book

--- lab_08_09/service_library/main.py
+++ lab_08_09/service_library/main.py
@@ -8,26 +8,29 @@ class LibraryApp(tk.Tk):
         super().__init__()
 
         self.title("Library Management System")
-        self.geometry("1000x500")
+        self.geometry("1500x1000")
         self.books = []
 
-        # self.label_author = tk.Label(self, text="Author:")
-        # self.label_author.grid(row=0, column=0, padx=5, pady=5)
-        # self.entry_author = tk.Entry(self)
-        # self.entry_author.grid(row=0, column=1, padx=5, pady=5)
-        #
-        # self.label_title = tk.Label(self, text="Title:")
-        # self.label_title.grid(row=1, column=0, padx=5, pady=5)
-        # self.entry_title = tk.Entry(self)
-        # self.entry_title.grid(row=1, column=1, padx=5, pady=5)
-        #
-        # self.label_pages = tk.Label(self, text="Pages:")
-        # self.label_pages.grid(row=2, column=0, padx=5, pady=5)
-        # self.entry_pages = tk.Entry(self)
-        # self.entry_pages.grid(row=2, column=1, padx=5, pady=5)
+        self.label_author = tk.Label(self, text="Author:")
+        self.label_author.grid(row=0, column=19, padx=5, pady=5)
+
+        self.entry_author = tk.Entry(self)
+        self.entry_author.grid(row=0, column=20, padx=5, pady=5)
+
+        self.label_title = tk.Label(self, text="Title:")
+        self.label_title.grid(row=1, column=19, padx=5, pady=5)
+
+        self.entry_title = tk.Entry(self)
+        self.entry_title.grid(row=1, column=20, padx=5, pady=5)
+
+        self.label_pages = tk.Label(self, text="Pages:")
+        self.label_pages.grid(row=2, column=19, padx=5, pady=5)
+
+        self.entry_pages = tk.Entry(self)
+        self.entry_pages.grid(row=2, column=20, padx=5, pady=5)
 
         self.button_add = tk.Button(self, text="Add Book", command=self.add_book)
-        self.button_add.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+        self.button_add.grid(row=1, column=7, columnspan=2)
 
         self.label_listbox_author = tk.Label(self, text="Author")
         self.label_listbox_author.grid(row=0, column=0, pady=5, sticky="ew")
@@ -48,22 +51,22 @@ class LibraryApp(tk.Tk):
         self.label_listbox_borrowed.grid(row=0, column=5, pady=5, sticky="ew")
 
         self.listbox_title = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_title.grid(row=1, column=0, columnspan=1, pady=10, sticky="nsew")
+        self.listbox_title.grid(row=1, column=0, columnspan=1, pady=10, sticky="nsew", rowspan=1)
 
         self.listbox_author = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_author.grid(row=1, column=1, columnspan=1, pady=10, sticky="nsew")
+        self.listbox_author.grid(row=1, column=1, columnspan=1, pady=10, sticky="nsew", rowspan=1)
 
-        self.listbox_pages = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_pages.grid(row=1, column=2, columnspan=1, pady=10, sticky="nsew")
+        self.listbox_pages = tk.Listbox(self, width=5, height=25, font=('Arial', 16))
+        self.listbox_pages.grid(row=1, column=2, columnspan=1, pady=10, sticky="nsew", rowspan=1)
 
         self.listbox_created = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_created.grid(row=1, column=3, columnspan=1, pady=10, sticky="nsew")
+        self.listbox_created.grid(row=1, column=3, columnspan=1, pady=10, sticky="nsew", rowspan=1)
 
         self.listbox_updated = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_updated.grid(row=1, column=4, columnspan=1, pady=10, sticky="nsew")
+        self.listbox_updated.grid(row=1, column=4, columnspan=1, pady=10, sticky="nsew", rowspan=1)
 
-        self.listbox_borrowed = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_borrowed.grid(row=1, column=5, columnspan=1, pady=10, sticky="nsew")
+        self.listbox_borrowed = tk.Listbox(self, width=8, height=25, font=('Arial', 16))
+        self.listbox_borrowed.grid(row=1, column=5, columnspan=1, pady=10, sticky="nsew", rowspan=1)
 
         # self.button_remove = tk.Button(self, text="Remove Book", command=self.remove_book)
         # self.button_remove.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
@@ -93,8 +96,10 @@ class LibraryApp(tk.Tk):
             self.listbox_pages.insert(tk.END, f'{pages}')
             self.listbox_created.insert(tk.END, f'{created}')
             self.listbox_updated.insert(tk.END, f'{updated}')
-            self.listbox_borrowed.insert(tk.END, f'{borrowed}')
-
+            if borrowed:
+                self.listbox_borrowed.insert(tk.END, f'Tak')
+            else:
+                self.listbox_borrowed.insert(tk.END, f'Na stanie')
 
 def main():
     pass
