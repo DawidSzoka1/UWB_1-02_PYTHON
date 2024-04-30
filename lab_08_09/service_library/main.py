bf52ac0bf16665bbb3ae99c23d758f5d4b6425ee
DawidSzoka1 <oficer130@gmail.com>
2024-04-30 14:04:01 +0200

    GUI as a class

--- lab_08_09/service_library/main.py
+++ lab_08_09/service_library/main.py
@@ -1,13 +1,56 @@
 from customerservice import *
-from rejestbook import *
+import rejestbook
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Service Library")
-root.geometry("1000x500")
-root.configure(background="black")
-label = tk.Label(root, text="Hello, World!", fg="white", bg='black')
-label.pack()
+
+class LibraryApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Library Management System")
+
+        self.books = []
+
+        self.label_title = tk.Label(self, text="Title:")
+        self.label_title.grid(row=0, column=0, padx=5, pady=5)
+        self.entry_title = tk.Entry(self)
+        self.entry_title.grid(row=0, column=1, padx=5, pady=5)
+
+        self.label_author = tk.Label(self, text="Author:")
+        self.label_author.grid(row=1, column=0, padx=5, pady=5)
+        self.entry_author = tk.Entry(self)
+        self.entry_author.grid(row=1, column=1, padx=5, pady=5)
+
+        self.label_pages = tk.Label(self, text="Pages:")
+        self.label_pages.grid(row=2, column=0, padx=5, pady=5)
+        self.entry_pages = tk.Entry(self)
+        self.entry_pages.grid(row=2, column=1, padx=5, pady=5)
+
+        self.button_add = tk.Button(self, text="Add Book", command=self.add_book)
+        self.button_add.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+
+        self.listbox_books = tk.Listbox(self)
+        self.listbox_books.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+
+        # self.button_remove = tk.Button(self, text="Remove Book", command=self.remove_book)
+        # self.button_remove.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+        #
+        # self.load_books()
+
+    def add_book(self):
+        rejestbook.add_book(read_csv, self.entry_author.get(), self.entry_title.get(), int(self.entry_pages.get()))
+
+    def load_books(self):
+        self.listbox_books.delete(0, tk.END)
+        self.books = []
+
+        df = read_csv("books.csv",
+                      'ID', 'AUTHOR', 'TITLE', 'PAGES', 'CREATED', 'UPDATED', 'BORROWED')
+        for index, row in df.iterrows():
+            author, title, pages, created = row['AUTHOR'], row['TITLE'], row['PAGES'], row['CREATED']
+            updated, borrowed = row['UPDATED'], row['BORROWED']
+            self.books.append((title, author))
+            self.listbox_books.insert(tk.END, f"{title} by {author}")
 
 
 def main():
@@ -15,5 +58,5 @@ def main():
 
 
 if __name__ == '__main__':
-    root.mainloop()
+    LibraryApp().mainloop()
     main()
