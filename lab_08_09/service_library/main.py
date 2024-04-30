e49e1e10c6cadbf196777f5e8ea7f3a8eb1e421a
DawidSzoka1 <oficer130@gmail.com>
2024-04-30 14:33:49 +0200

    working load_books method

--- lab_08_09/service_library/main.py
+++ lab_08_09/service_library/main.py
@@ -8,18 +8,18 @@ class LibraryApp(tk.Tk):
         super().__init__()
 
         self.title("Library Management System")
-
+        self.geometry("1000x500")
         self.books = []
 
-        self.label_title = tk.Label(self, text="Title:")
-        self.label_title.grid(row=0, column=0, padx=5, pady=5)
-        self.entry_title = tk.Entry(self)
-        self.entry_title.grid(row=0, column=1, padx=5, pady=5)
-
         self.label_author = tk.Label(self, text="Author:")
-        self.label_author.grid(row=1, column=0, padx=5, pady=5)
+        self.label_author.grid(row=0, column=0, padx=5, pady=5)
         self.entry_author = tk.Entry(self)
-        self.entry_author.grid(row=1, column=1, padx=5, pady=5)
+        self.entry_author.grid(row=0, column=1, padx=5, pady=5)
+
+        self.label_title = tk.Label(self, text="Title:")
+        self.label_title.grid(row=1, column=0, padx=5, pady=5)
+        self.entry_title = tk.Entry(self)
+        self.entry_title.grid(row=1, column=1, padx=5, pady=5)
 
         self.label_pages = tk.Label(self, text="Pages:")
         self.label_pages.grid(row=2, column=0, padx=5, pady=5)
@@ -29,28 +29,34 @@ class LibraryApp(tk.Tk):
         self.button_add = tk.Button(self, text="Add Book", command=self.add_book)
         self.button_add.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
-        self.listbox_books = tk.Listbox(self)
-        self.listbox_books.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+        self.listbox_books = tk.Listbox(self, width=50)
+        self.listbox_books.grid(row=4, column=0, columnspan=4, padx=5, pady=5)
 
         # self.button_remove = tk.Button(self, text="Remove Book", command=self.remove_book)
         # self.button_remove.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
         #
-        # self.load_books()
+        self.load_books()
 
     def add_book(self):
         rejestbook.add_book(read_csv, self.entry_author.get(), self.entry_title.get(), int(self.entry_pages.get()))
+        self.load_books()
 
     def load_books(self):
-        self.listbox_books.delete(0, tk.END)
-        self.books = []
-
-        df = read_csv("books.csv",
+        df = read_csv("Library/book.csv",
                       'ID', 'AUTHOR', 'TITLE', 'PAGES', 'CREATED', 'UPDATED', 'BORROWED')
+        if type(df) is not pd.DataFrame:
+            self.listbox_books.insert(tk.END, f"{df}")
+            return 0
+
         for index, row in df.iterrows():
-            author, title, pages, created = row['AUTHOR'], row['TITLE'], row['PAGES'], row['CREATED']
+            author, title = row['AUTHOR'], row['TITLE']
+            for book in self.books:
+                if author in book and title in book:
+                    return 0
+            pages, created = row['PAGES'], row['CREATED']
             updated, borrowed = row['UPDATED'], row['BORROWED']
-            self.books.append((title, author))
-            self.listbox_books.insert(tk.END, f"{title} by {author}")
+            self.books.append((author, title, pages, created, updated, borrowed))
+            self.listbox_books.insert(tk.END, f"{author}, {title}, {pages}, {created}, {updated}, {borrowed}")
 
 
 def main():
