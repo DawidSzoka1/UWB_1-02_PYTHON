dac4f1196e5d1d2e46b5e6ee0e851d1474aa5fab
DawidSzoka1 <oficer130@gmail.com>
2024-05-01 15:33:21 +0200

    decorator finished

--- lab_08_09/service_library/decorator.py
+++ lab_08_09/service_library/decorator.py
@@ -1,7 +1,5 @@
-from customerservice import return_book
-
-
 def decorator(func):
-    def wrapper(*args):
-        pass
+    def wrapper(customer_id, *args):
+        check = list(map(lambda title: func(customer_id, title), args))
+        return 1 if all(check) else 0
     return wrapper
