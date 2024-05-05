04350abd8c37fa7cd5ffea3d853ac89ec68d0204
DawidSzoka1 <oficer130@gmail.com>
2024-05-05 17:51:30 +0200

    decorator changes

--- lab_08_09/service_library/decorator.py
+++ lab_08_09/service_library/decorator.py
@@ -1,5 +1,15 @@
 def decorator(func):
     def wrapper(customer_id, *args):
+        return_div = {'type': 'error'}
         check = list(map(lambda title: func(customer_id, title), args))
-        return 1 if all(check) else 0
+        all_success = all(item['type'] == 'success' for item in check)
+        if all_success:
+            return_div['type'] = 'success'
+            return_div['message'] = 'All books were successfully returned'
+            return return_div
+        error_messages = list(filter(lambda text: text != '',
+                                         [item['message'] if item['type'] == 'error' else '' for item in
+                                          check]))
+        return_div['message'] = f'Some error with borrowing books: {", ".join(error_messages)}'
+        return return_div
     return wrapper
