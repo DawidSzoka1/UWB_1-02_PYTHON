47cda0495844083e51a62ea4b601fd1f0a8c7721
DawidSzoka1 <oficer130@gmail.com>
2024-05-06 10:13:59 +0200

    two frames and some buttons

--- lab_08_09/service_library/main.py
+++ lab_08_09/service_library/main.py
@@ -14,70 +14,34 @@ class LibraryApp(tk.Tk):
         self.geometry("1500x1000")
         self.books = []
 
-        self.label_author = tk.Label(self, text="Author:")
-        self.label_author.grid(row=0, column=19, padx=5, pady=5)
+        self.frame1 = tk.Frame(self, bg="#08172B", width=800, height=1000)
+        self.frame1.grid(row=0, column=0, sticky="nsew")
 
-        self.entry_author = tk.Entry(self)
-        self.entry_author.grid(row=0, column=20, padx=5, pady=5)
+        self.text_label = tk.Label(self.frame1, bg="#08172B", text="LIBRARY", fg="#CFCFA7", font=("Georgia pro", 20))
+        self.text_label.grid(row=0, column=0)
+        self.frame2 = tk.Frame(self, bg="black", width=200, height=1000)
+        self.frame2.grid(row=0, column=1, rowspan=2, sticky="nsew")
 
-        self.label_title = tk.Label(self, text="Title:")
-        self.label_title.grid(row=1, column=19, padx=5, pady=5)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=2)
+        self.bind("<Configure>", self.on_window_resize)
 
-        self.entry_title = tk.Entry(self)
-        self.entry_title.grid(row=1, column=20, padx=5, pady=5)
+        self.add_book_button = tk.Button(self.frame2, text="ADD \nBOOK", width=45, height=5, command=self.add_book,
+                                         background="black", fg="white", bd=5, relief="raised",
+                                         highlightbackground='white', )
+        self.add_book_button.grid(row=0, column=2, sticky="nsew", padx=50, pady=(200, 10))
 
-        self.label_pages = tk.Label(self, text="Pages:")
-        self.label_pages.grid(row=2, column=19, padx=5, pady=5)
+        self.add_user_button = tk.Button(self.frame2, text="ADD \nUSER", width=45, height=5,
+                                         background="black", fg="white")
+        self.add_user_button.grid(row=1, column=2, pady=(10, 200), padx=50)
 
-        self.entry_pages = tk.Entry(self)
-        self.entry_pages.grid(row=2, column=20, padx=5, pady=5)
+        # self.load_books()
 
-        self.button_add = tk.Button(self, text="Add Book", command=self.add_book)
-        self.button_add.grid(row=1, column=7, columnspan=2)
-
-        self.label_listbox_author = tk.Label(self, text="Author")
-        self.label_listbox_author.grid(row=0, column=0, pady=5, sticky="ew")
-
-        self.label_listbox_title = tk.Label(self, text="Title")
-        self.label_listbox_title.grid(row=0, column=1, pady=5, sticky="ew")
-
-        self.label_listbox_pages = tk.Label(self, text="Pages")
-        self.label_listbox_pages.grid(row=0, column=2, pady=5, sticky="ew")
-
-        self.label_listbox_created = tk.Label(self, text="CREATED")
-        self.label_listbox_created.grid(row=0, column=3, pady=5, sticky="ew")
-
-        self.label_listbox_updated = tk.Label(self, text="UPDATED")
-        self.label_listbox_updated.grid(row=0, column=4, pady=5, sticky="ew")
-
-        self.label_listbox_borrowed = tk.Label(self, text="BORROWED")
-        self.label_listbox_borrowed.grid(row=0, column=5, pady=5, sticky="ew")
-
-        self.listbox_author = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_author.grid(row=1, column=0, columnspan=1, pady=10, sticky="nsew", rowspan=1)
-
-        self.listbox_title = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_title.grid(row=1, column=1, columnspan=1, pady=10, sticky="nsew", rowspan=1)
-
-        self.listbox_pages = tk.Listbox(self, width=5, height=25, font=('Arial', 16))
-        self.listbox_pages.grid(row=1, column=2, columnspan=1, pady=10, sticky="nsew", rowspan=1)
-
-        self.listbox_created = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_created.grid(row=1, column=3, columnspan=1, pady=10, sticky="nsew", rowspan=1)
-
-        self.listbox_updated = tk.Listbox(self, width=10, height=25, font=('Arial', 16))
-        self.listbox_updated.grid(row=1, column=4, columnspan=1, pady=10, sticky="nsew", rowspan=1)
-
-        self.listbox_borrowed = tk.Listbox(self, width=8, height=25, font=('Arial', 16))
-        self.listbox_borrowed.grid(row=1, column=5, columnspan=1, pady=10, sticky="nsew", rowspan=1)
-
-        self.button_remove = tk.Button(self, text="Remove Book", command=self.delete_book)
-        self.button_remove.grid(row=7, column=0, columnspan=2, padx=5, pady=5)
-        self.label_remove_id = tk.Label(self, text="book_id")
-        self.label_remove_id.grid(row=4, column=0, padx=5, pady=5)
-        self.remove_id = tk.Entry(self)
-        self.remove_id.grid(row=5, column=0, padx=5, pady=5)
-        self.load_books()
+    def on_window_resize(self, event):
+        window_height = self.winfo_height()
+        window_width = self.winfo_width()
+        self.frame1.configure(height=window_height, width=window_width*66/100)
+        self.frame2.configure(height=window_height, width=window_width*34/100)
 
     def add_book(self):
         call_back = rejestbook.add_book(read_csv, self.entry_author.get(), self.entry_title.get(), int(self.entry_pages.get()))
@@ -85,7 +49,7 @@ class LibraryApp(tk.Tk):
             messagebox.showerror("Error", call_back['message'])
         else:
             messagebox.showinfo("Success", call_back['message'])
-        self.load_books()
+            self.load_books()
 
     def load_books(self):
         df = read_csv("Library/book.csv",
