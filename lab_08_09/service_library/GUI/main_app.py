b4b5b8cd3980b134a639e69fd5b78ab5e72509f7
DawidSzoka1 <oficer130@gmail.com>
2024-05-07 17:23:06 +0200

    actions frame done

--- lab_08_09/service_library/GUI/main_app.py
+++ lab_08_09/service_library/GUI/main_app.py
@@ -33,7 +33,7 @@ class Application(tk.Frame):
         self.frame_list.grid(row=0, column=0, sticky='nsew')
         self.frame_list.grid_propagate(False)
         self.label_frame_list()
-        self.create_buttons_list()
+        self.list_redirect()
 
     def label_frame_list(self):
         self.text_label = tk.Label(
@@ -45,9 +45,9 @@ class Application(tk.Frame):
         )
         self.text_label.grid(row=0, column=0, padx=(0, 250), pady=(50, 0), columnspan=4)
 
-    def create_buttons_list(self):
+    def list_redirect(self):
         self.books_redirect = tk.Button(
-            border_func(self.frame_list, 1, 2, (150, 10), (0, 250), self.font_color_1, self.bg_color_1),
+            border_func(self.frame_list, 1, 2, (150, 10), (0, 300), self.font_color_1, self.bg_color_1),
             bg="#08172B", width=20,
             text="AVAILABLE BOOKS",
             fg=self.font_color_1,
@@ -56,7 +56,7 @@ class Application(tk.Frame):
         self.books_redirect.grid(row=1, column=0)
 
         self.borrowed_books_redirect = tk.Button(
-            border_func(self.frame_list, 2, 2, 10, (0, 250), self.font_color_1, self.bg_color_1),
+            border_func(self.frame_list, 2, 2, 10, (0, 300), self.font_color_1, self.bg_color_1),
             bg="#08172B",
             text="BORROWED BOOKS", fg=self.font_color_1,
             width=20,
@@ -65,7 +65,7 @@ class Application(tk.Frame):
         self.borrowed_books_redirect.grid(row=2, column=0)
 
         self.customer_list_redirect = tk.Button(
-            border_func(self.frame_list, 3, 2, (10, 150), (0, 250), self.font_color_1, self.bg_color_1),
+            border_func(self.frame_list, 3, 2, (10, 150), (0, 300), self.font_color_1, self.bg_color_1),
             bg="#08172B", width=20,
             text="CUSTOMERS LIST",
             fg=self.font_color_1,
@@ -74,14 +74,58 @@ class Application(tk.Frame):
         self.customer_list_redirect.grid(row=3, column=0)
 
     def create_frame_actions(self):
-        frame_actions = tk.Frame(
+        self.frame_actions = tk.Frame(
             self.main_frame,
             background=self.bg_color_2,
             width=500,
             height=self.main_frame.winfo_height())
-        frame_actions.grid_columnconfigure(1, weight=1)
-        frame_actions.grid(row=0, column=1, sticky='nsew')
-        frame_actions.grid_propagate(False)
+        self.frame_actions.grid_columnconfigure(1, weight=1)
+        self.frame_actions.grid(row=0, column=1, sticky='nsew')
+        self.frame_actions.grid_propagate(False)
+        self.redirect_actions()
+
+    def redirect_actions(self):
+        self.add_book_redirect = tk.Button(
+            border_func(self.frame_actions, 0, 0, (100, 10), (80, 0), color=self.font_color_2, bg_color=self.bg_color_2),
+            text="ADD \nBOOK",
+            width=20,
+            height=5,
+            background=self.bg_color_2,
+            fg=self.font_color_2,
+            font=(self.font_style, 20, "bold"))
+        self.add_book_redirect.grid(row=0, column=0)
+
+        self.add_user_redirect = tk.Button(
+            border_func(self.frame_actions, 1, 0, 5, (80, 0), color=self.font_color_2, bg_color=self.bg_color_2),
+            text="ADD \nUSER",
+            width=20,
+            height=5,
+            background=self.bg_color_2,
+            fg=self.font_color_2,
+            font=(self.font_style, 20, "bold")
+        )
+        self.add_user_redirect.grid(row=0, column=0)
+        self.borrow_book_redirect = tk.Button(
+            border_func(self.frame_actions, 2, 0, 5, (80, 0), color=self.font_color_2, bg_color=self.bg_color_2),
+            text="BORROW \nBOOK",
+            width=20,
+            height=5,
+            background=self.bg_color_2,
+            fg=self.font_color_2,
+            font=(self.font_style, 20, "bold")
+        )
+        self.borrow_book_redirect.grid(row=0, column=0)
+
+        self.return_book_redirect = tk.Button(
+            border_func(self.frame_actions, 3, 0, 5,  (80, 0), color=self.font_color_2, bg_color=self.bg_color_2),
+            text="RETURN \nBOOK",
+            width=20,
+            height=5,
+            background=self.bg_color_2,
+            fg=self.font_color_2,
+            font=(self.font_style, 20, "bold")
+        )
+        self.return_book_redirect.grid(row=0, column=0)
 
 
 root = tk.Tk()
