2b5ec108640b3294357c4e0a49cea86548f558b8
DawidSzoka1 <oficer130@gmail.com>
2024-05-09 15:20:24 +0200

    some layout and working redirections

--- lab_08_09/service_library/GUI/home_page.py
+++ lab_08_09/service_library/GUI/home_page.py
@@ -115,7 +115,8 @@ class MainPage(tk.Frame):
             height=5,
             background=self.parent.bg_color_2,
             fg=self.parent.font_color_2,
-            font=(self.parent.font_style, 20, "bold")
+            font=(self.parent.font_style, 20, "bold"),
+            command=lambda: self.parent.show_frame(page=6)
         )
         self.borrow_book_redirect.grid(row=0, column=0)
 
--- lab_08_09/service_library/GUI/main_app.py
+++ lab_08_09/service_library/GUI/main_app.py
@@ -4,6 +4,7 @@ from lab_08_09.service_library.GUI.home_page import MainPage
 from lab_08_09.service_library.GUI.all_customers import Customers
 from lab_08_09.service_library.GUI.add_book_page import AddBookPage
 from lab_08_09.service_library.GUI.add_user_page import AddUserPage
+from lab_08_09.service_library.GUI.borrow_book_page import BorrowBookPage
 
 
 class Application(tk.Frame):
@@ -13,7 +14,7 @@ class Application(tk.Frame):
         self.font_color_1 = '#CFCFA7'
         self.font_color_2 = 'white'
         self.font_style = "Georgia pro"
-        self.pages = [MainPage, Books, Books, Customers, AddBookPage, AddUserPage]
+        self.pages = [MainPage, Books, Books, Customers, AddBookPage, AddUserPage, BorrowBookPage]
 
         super().__init__(root, bg=self.bg_color_1)
 
