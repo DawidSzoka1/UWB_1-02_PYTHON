28a65eb1cc072014c80a8eaafcb8a38c6863745c
DawidSzoka1 <oficer130@gmail.com>
2024-05-21 12:51:36 +0200

    ex 2

--- LAB_10.py
+++ LAB_10.py
@@ -13,9 +13,14 @@ from PIL import Image, ImageTk
 #     try:
 #         num1 = float(a.get())
 #         num2 = float(b.get())
+#         num1 / num2
 #     except ValueError:
 #         showerror('Wrong input', 'Please enter valid numbers')
 #         return 0
+#     except ZeroDivisionError:
+#         showerror('Wrong input', 'cant divide by 0')
+#         return 0
+#
 #     if var.get() == 1:
 #         result_operation = f'{num1 + num2}'
 #     elif var.get() == 2:
@@ -56,6 +61,26 @@ from PIL import Image, ImageTk
 # # Uwaga jak wiadomo emocje można wyrazić za pomocą kolorów
 # # A zatem postaraj się uzależnić kolor wyświetlanej kwoty raty do spłaty od potencjalnych emocji klienta banku na
 # # widok ile musi oddać bankowi.
+def calculate():
+    q = (1+float(p.get())) / 100
+    rata = round((float(k.get())*q**int(n.get())*(1-q))/(1-q**int(n.get())), 2)
+    result.config(text=f'rata wynosi {rata}')
+
+root = Tk()
+root.geometry("500x500")
+Label(root, text="Kwoata kredytu").grid(row=0, column=0)
+k = Entry(root)
+k.grid(row=1, column=0)
+Label(root, text="Liczba okresowe").grid(row=2, column=0)
+n = Entry(root)
+n.grid(row=3, column=0)
+Label(root, text="Stopa procentowa").grid(row=4, column=0)
+p = Entry(root)
+p.grid(row=5, column=0)
+Button(root, text='oblicz', command=calculate).grid(row=6, column=0)
+result = Label(root, text="")
+result.grid(row=7, column=0)
+root.mainloop()
 #
 # # #########Zadanie 3
 # # Pewna firma zleciła Ci wykonie badania ankietowego dotyczącego kupowanych przez konsumentów produktów na święta:
